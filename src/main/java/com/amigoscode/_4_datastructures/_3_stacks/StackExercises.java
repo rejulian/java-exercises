f172e5f06a8b157874336f61520e57617b4b4da7
rejulian <contacto.julian.re@gmail.com>
2026-07-18 19:17:53 -0400

    stack exercises.

--- src/main/java/com/amigoscode/_4_datastructures/_3_stacks/StackExercises.java
+++ src/main/java/com/amigoscode/_4_datastructures/_3_stacks/StackExercises.java
@@ -10,22 +10,27 @@ public class StackExercises {
     public static void main(String[] args) {
 
         // TODO: 1 - Create a Stack of Strings called 'stack'
-
+        Stack<String> stack = new Stack<>();
 
         // TODO: 2 - Push 5 elements onto the stack: "Java", "Python", "C++", "JavaScript", "Go"
-
+        stack.push("Java");
+        stack.push("Python");
+        stack.push("C++");
+        stack.push("JavaScript");
+        stack.push("Go");
 
         // TODO: 3 - Peek at the top element without removing it
         //           Print the result (should be "Go")
-
+        System.out.println(stack.peek());
 
         // TODO: 4 - Pop an element from the stack and print it
         //           Then print the stack to see the remaining elements
-
+        System.out.println(stack.pop());
+        System.out.println(stack);
 
         // TODO: 5 - Check if the stack is empty using isEmpty()
         //           Print the result
-
+        System.out.println(stack.isEmpty());
 
         // --- String Reversal ---
         System.out.println("\n--- String Reversal ---");
@@ -33,7 +38,17 @@ public class StackExercises {
         // TODO: 6 - Use a Stack to reverse the string 'original'
         //           Push each character onto a stack, then pop them all to build the reversed string
         //           Print both original and reversed strings
+        Stack<Character> textStack = new Stack<>();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = 0; i < original.length(); i++) {
+            textStack.push(original.charAt(i));
+        }
 
+        while (!textStack.isEmpty()) {
+            reversed.append(textStack.pop());
+        }
+        System.out.println(original);
+        System.out.println(reversed);
 
         // --- Balanced Brackets ---
         System.out.println("\n--- Balanced Brackets ---");
